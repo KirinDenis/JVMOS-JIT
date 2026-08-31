f66b67f9f0237f9faa268fba2ff4718dc88eb88c
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-08-30 19:49:51 -0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- jvm/bootjvm.asm
+++ jvm/bootjvm.asm
@@ -1,25 +1,3 @@
-; MIT License
-;
-; Copyright (c) 2026 Allan (Slam)
-;
-; Permission is hereby granted, free of charge, to any person obtaining a copy
-; of this software and associated documentation files (the "Software"), to deal
-; in the Software without restriction, including without limitation the rights
-; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-; copies of the Software, and to permit persons to whom the Software is
-; furnished to do so, subject to the following conditions:
-;
-; The above copyright notice and this permission notice shall be included in all
-; copies or substantial portions of the Software.
-;
-; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-; SOFTWARE.
-
 [bits 32]
 
 global bootjvm_start
@@ -453,7 +431,6 @@ resolve_and_compile_java_method:
     mov esi, [ebp - 4]                  
     mov ecx, [ebp - 8]                  
 
-	; Hack que impide que se lean otros archivos .class desde el grub
     ;cmp dword [esi], 0x6176616A         ; "java"
     ;je .bypass_method
 
